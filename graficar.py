2053f2edf01ad105674933d83aef4e4632f8f1f9
JuveLVH <velasquez.luis.cbtis37@gmail.com>
2023-05-23 08:53:29 -0700

    cambio en las graficaas

--- graficar.py
+++ graficar.py
@@ -1,8 +1,3 @@
-'''
-    Archivo para crear graficas, es solo para ejecutarse en computadora personal.
-# param 1: Bandera que dice si se usan los ultimos datos para graficar  
-'''
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,10 +11,19 @@ import easygui as eg
 
 
 #----------------------------------------------------------------------------
-#-------------------- Se busca el archivo con los datos ---------------------
+#-------------------- Se buscan los archivo con los datos ---------------------
 #----------------------------------------------------------------------------
 opcion = "s"
 direcciones_datos = []
+nombre_datos = []
+etiquetas_datos = []
+
+print("Porfavor eliga el archivo con los datos secuenciales ...")
+archivos_datos_secuencial = eg.fileopenbox(
+    msg="Elije el archivo con los tiempos",
+    title="Control: fileopenbox",
+    default='//sshfs/ppd@148.225.111.150/juventino_velasquez_ocotillo/')
+ 
 
 while(opcion == "s" ):
     print("Porfavor este atento de la ventana emergente ...")
@@ -28,16 +32,20 @@ while(opcion == "s" ):
         title="Control: fileopenbox",
         default='//sshfs/ppd@148.225.111.150/juventino_velasquez_ocotillo/')
     direcciones_datos.append(direccion)
+    descripcion_datos = input("Descripcion con la que los datos aparecen en la grafica: ")
+    etiquetas_datos.append(descripcion_datos)
     opcion = input("Quieres añadir más archivos? (s/n) : ")
-
 
 #----------------------------------------------------------------------------
 #------------------------ Se prepara la informacion  ------------------------
 #----------------------------------------------------------------------------
-tiempos_secuenciales = []
 tiempos_paralelos = []
 tiempos_speedup = []
 labels_paralelos = []
+etiquetas_bien_ordenadas = []
+
+tiempo_ejecucion_secuencial = pd.read_csv(archivos_datos_secuencial , sep=',',  names=['nh','tiempo','aux'])
+tiempo_secuencial = tiempo_ejecucion_secuencial['tiempo'][0]
 
 while(len(direcciones_datos) != 0):
     
@@ -46,52 +54,50 @@ while(len(direcciones_datos) != 0):
     
     #Datos en crudo
     tiempos_ejecucion = pd.read_csv(direccion_resultados , sep=',',  names=['nh','tiempo','aux'])
+    etiquetas_bien_ordenadas.append(etiquetas_datos.pop())
 
     #Datos para tiempos de ejecuccion
-    tiempo_secuencial_local = tiempos_ejecucion['tiempo'][0]
-    tiempos_secuenciales.append(tiempo_secuencial_local)
-    
-    tiempo_paralelo_local = tiempos_ejecucion['tiempo'][1:]
+    tiempo_paralelo_local = tiempos_ejecucion['tiempo'][:]
     tiempos_paralelos.append(tiempo_paralelo_local)
-
-    labels_paralelos.append(tiempos_ejecucion['nh'][1:])
-
+    labels_paralelos.append(tiempos_ejecucion['nh'][:])
+    
     #Datos para el speedup
     tiempo_speedup = []
     for tiempo in tiempo_paralelo_local:
-        tiempo_speedup.append(tiempo_secuencial_local / tiempo)
+        tiempo_speedup.append(tiempo_secuencial / tiempo)
     
     tiempos_speedup.append(tiempo_speedup)
 
 #----------------------------------------------------------------------------
 #------------------------- Se grafican los tiempos --------------------------
 #----------------------------------------------------------------------------
-
 titulo_tiempos = input("Titulo de la grafica de tiempos: ")
 titulo_speedup = input("Titulo de la grafica del speedup: ")
+
 with plt.style.context('seaborn-darkgrid'):
     with plt.style.context('seaborn-darkgrid'):
         diagrama_speedup, grafica_speedup = plt.subplots(1)
         diagrama_tiempos , grafica_tiempos = plt.subplots(1)
+        
+        grafica_tiempos.plot(1,tiempo_secuencial,
+                    color='r',
+                    marker='*',
+                    markersize=15, 
+                    label = 'Tiempo secuencial')
 
-        while len(tiempos_secuenciales) != 0  :
+        while len(tiempos_paralelos) != 0  :
 #----------------------------------------------------------------------------
 #-------------------------- Se grafica el tiempo ---------------------------
 #----------------------------------------------------------------------------
-            tiempo_secuencial = tiempos_secuenciales.pop()
             tiempo_paralelo = tiempos_paralelos.pop()
             labels_paralelo = labels_paralelos.pop() 
             labels_speedup = labels_paralelo
             tiempo_speedup = tiempos_speedup.pop() 
+            etiqueta = etiquetas_bien_ordenadas.pop() 
 
-            grafica_tiempos.plot(1,tiempo_secuencial,
-                        color='r',
-                        marker='*',
-                        markersize=15, 
-                        label = 'Tiempo secuencial')
             grafica_tiempos.plot(labels_paralelo ,tiempo_paralelo ,
                         marker='.',
-                        label='Variacion del tiempo usando hilos',
+                        label=etiqueta,
                         linestyle='dashed')
             grafica_tiempos.set_xlabel("Número de hilos")
             grafica_tiempos.set_ylabel("Tiempo")
@@ -102,7 +108,7 @@ with plt.style.context('seaborn-darkgrid'):
 #-------------------------- Se grafica el speedup ---------------------------
 #----------------------------------------------------------------------------
             grafica_speedup.plot(labels_speedup ,tiempo_speedup ,
-                        label='Speedup segun el numero de hilos')
+                        label=etiqueta)
             grafica_speedup.set_xlabel("Número de hilos")
             grafica_speedup.set_ylabel("SpeedUp")
             grafica_speedup.set_title(titulo_speedup)
